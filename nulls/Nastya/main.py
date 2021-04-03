836ef681398b83c9d1d561be9d23e0051e39e434
shurik <shurik@skotch.nirvana>
2021-04-03 17:59:48 +0300

    8

---
+++ nulls/Nastya/main.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python3
+
+
+#Импорт всякого очень интересного и не очень
+import sys
+from PyQt5.QtWidgets import QApplication
+
+from PyQt5.QtWidgets import (QMainWindow, QWidget,
+        QPushButton, QLineEdit, QInputDialog, QDialog, QDialogButtonBox, QComboBox, QCalendarWidget,
+        QFormLayout, QLabel, QSpinBox, QTreeView, QVBoxLayout, QHBoxLayout)
+
+from PyQt5 import QtCore
+from PyQt5.QtGui import QImage
+
+
+#Импортим файл myDatabase.py который написали сами потому что мы могем
+import myDatabase
+
+
+class Msg:
+    def set_img(self, img, f = None):
+        self.f = f
+        self.img = img
+        self.date = QtCore.QDateTime.currentDateTime()
+    def set_text(self, text, f = None):
+        self.f = f
+        self.text = text
+        self.date = QtCore.QDateTime.currentDateTime()
+    def is_text(self):
+        return self.text != None
+    def is_img(self):
+        return self.img != None
+
+class Message(QWidget):
+    def __init__(self, msg, parent=None):
+        super().__init__(parent=parent)
+        self.msg = msg
+        #лайоут это кароч такая штука которая рамещает на себе виджеты так чтоб их размер не зависил он конкретных размеров окна
+        #лайоут все шо надо растянет или сожмет что все по красоте было
+        layout = QFormLayout()
+        #Поставим лайоут по методу родителя
+        super().setLayout(layout)
+        if msg.is_text():
+            #Лайоут был особый, пацанский поэтому заполняем его вот так вот
+            layout.addRow(QLabel(msg.text), QLabel(msg.date.toString()))
+        elif msg.is_img():
+            layout.addRow(msg.img, QLabel(msg.date.toString()))
+
+
+
+class Dialog(QWidget):
+    def __init__(self, msg, author, func, parent=None):
+        super().__init__(parent=parent)
+        self.func = func
+        self.msg = msg
+        self.author = author
+        layout = QHBoxLayout()
+        super().setLayout(layout)
+        author_label = QLabel(author)
+        layout.addWidget(author_label)
+        layout.addWidget(self.msg)
+
+    doubleClicked = QtCore.pyqtSignal()
+    def mouseDoubleClickEvent(self, event):
+        self.doubleClicked.emit()
+
+        
+
+
+
+#Чтоб запрашивать данные от пользователя разом напишем диалог
+#Диалог он потому что нужно меньше всего переопределять и он работает из коробки
+#А главное он просит у пользователя accept или reject и кароч это круто
+class myDialog(QWidget):
+    #Тупа конструктор
+    #l - лист типа [(надпись, виджет с которого будем получать ввод), ...] title - заголовок окошка
+    def __init__(self, msgs, title = "question", parent = None):
+        #Конструктор родителя а именно КуТэДиалога. super() - это типа батя
+        super().__init__(parent, QtCore.Qt.Window)
+        #Метод установки заголовка он у родителя поэтому от родителя его и дернем
+        self.setWindowTitle(title)
+        #лайоут это кароч такая штука которая рамещает на себе виджеты так чтоб их размер не зависил он конкретных размеров окна
+        #лайоут все шо надо растянет или сожмет что все по красоте было
+        layout = QVBoxLayout()
+        #Поставим лайоут по методу родителя
+        super().setLayout(layout)
+        self.msg_layout = QVBoxLayout()
+        tmp_layout = QHBoxLayout()
+
+        for msg in msgs:
+            self.msg_layout.addWidget(msg)
+
+        layout.addLayout(self.msg_layout)
+        layout.addLayout(tmp_layout)
+        self.send = QPushButton("Send")
+        self.text = QLineEdit()
+        self.attach = QPushButton("Send Pic")
+        tmp_layout.addWidget(self.text)
+        tmp_layout.addWidget(self.attach)
+        tmp_layout.addWidget(self.send)
+
+
+
+
+
+class MainWindow(QMainWindow):
+    #Тупа конструктор который получает имя бд
+    def __init__(self, dataBaseName):
+        #Тоже что и выше
+        super().__init__()
+        #Откроем бд и запомним ее в _myDatabase
+        self._myDatabase = myDatabase.MyDataBase(dataBaseName)
+
+        #Размеры окошка ширина, высота, позиция от левого края экрана, от правого
+        self.setGeometry(300,300,200,200)
+        #Тупа заголовок
+        self.setWindowTitle('Messanger')
+        #Ниже надо раскоментить чтоб поставить иконку
+        #self.setWindowIcon(QIcon('Файл с иконкой'))
+
+        #это на будущее
+        w = QWidget()
+
+        #Вертикальный лайоут так хочу так и будет
+        mainLayout = QVBoxLayout()
+        w.setLayout(mainLayout)
+
+        #Вот оно будущее. Специфика QMainWindow
+        self.setCentralWidget(w)
+        
+        #dialogs = self._myDatabase.get_dialogs()
+        
+        def create_d(text, author):
+            m = Msg()
+            m.set_text(text)
+            msg = Message(m)
+            return Dialog(msg, author, lambda x : print(x) )
+        dialogs = [create_d("aaa", "bb"), create_d("ccc", "ff")]
+
+        #Пичкуем наши лайоуты всем чем надо
+        for d in dialogs:
+            d.doubleClicked.connect(self.create_my_dialog)
+            mainLayout.addWidget(d)
+
+    def create_my_dialog(self):
+        d = self.sender()
+
+        #msgs = self._myDatabase.get_msg_from_author(d.author)
+
+        msgs = []
+
+        m = Msg()
+        m.set_text("aaa")
+        msgs.append(Message(m))
+        m.set_text("bbb")
+        msgs.append(Message(m))
+
+        self.md = myDialog(msgs, title=d.author, parent=self)
+        self.md.show()
+
+
+
+#Это то как все делают чтоб при импорте данного файла куда-то ничего не работало а работало тока тута
+if __name__ == "__main__":
+    #тут все как положено трогать ничего нельзя потому что так принято
+    app = QApplication(sys.argv)
+
+    w = MainWindow("test.bd")
+    w.show()
+
+    sys.exit(app.exec_())
+
+
---
+++ nulls/Nastya/myDatabase.py
@@ -0,0 +1,180 @@
+
+import datetime
+
+from peewee import *
+
+from PyQt5 import QtCore
+from PyQt5.QtCore import QAbstractTableModel, QVariant
+
+#Пишем свою модель потому что там проще и она будет наследником QAbst...TableModel по той же причине
+class MyModel(QAbstractTableModel):
+    #Тупа конструктор
+    def __init__(self, items, labels):
+        super().__init__()
+        #Скопируем че нада
+        #Итемы тут [[00, 01, ...], [10, 11, ...], ...]
+        self.list = items.copy()
+        #Это названия колонок
+        self.colLabels = labels.copy()
+
+    #Так принято прегружать виртуальные методы 
+    def rowCount(self, parent):
+        return len(self.list)
+
+    def columnCount(self, parent):
+        return len(self.colLabels)
+    
+    def headerData(self, section, orientation, role):
+        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
+            return QVariant(self.colLabels[section])
+        return QVariant()
+
+    #Тут тоже так принято
+    def data(self, index, role):
+        if not index.isValid() or role != QtCore.Qt.DisplayRole and role != QtCore.Qt.EditRole:
+            return QVariant()
+        val = ''
+        if role == QtCore.Qt.DisplayRole:
+            try:
+                #Мы не знаем как на передали список и шо там поэтому так
+                tmp = self.list[index.row()]
+                #Чтоб было одинковый интерфейс сделаем тупле если внутри список то он станет туплом если там соло итем то он станет туплом
+                val = tuple(tmp)[index.column()]
+            except IndexError:
+                pass
+        return val
+
+
+
+
+
+
+#Так надо читай доки
+mainDatabase = SqliteDatabase(None)
+
+#Чтоб в каждый класс не писать
+class BaseModel(Model):
+    class Meta:
+        #Да глобальная переменная определит классы. Так надо
+        database = mainDatabase
+
+#Класс - Таблица в ней поля
+class Store(BaseModel):
+    #Чар поле с именем "name"
+    name = CharField()
+    #Это поле пусть уникально
+    adress = CharField(unique = True)
+
+class Product(BaseModel):
+    name = CharField(unique = True)
+    praise = IntegerField()
+    #store - Это ключе к таблице Store может быть пустым поросто так
+    store = ForeignKeyField(Store, null = True)
+
+class Storage(BaseModel):
+    adress = CharField(unique = True)
+    #backref - значит что у Product появится ключк storage к Storage
+    product = ForeignKeyField(Product, backref="storage")
+
+class Rate(BaseModel):
+    author = CharField()
+    stars = IntegerField()
+    product = ForeignKeyField(Product)
+
+class Supply(BaseModel):
+    product = ForeignKeyField(Product)
+    count = IntegerField()
+    date = DateTimeField()
+
+class Cashier(BaseModel):
+    store = ForeignKeyField(Store)
+    name = CharField(unique = True)
+    salary = IntegerField()
+
+
+#Заполним первый магазин
+def initDatabase1():
+    tmp_store = Store.create(name = "6shesterechka", adress = "prospect nezavisimosti 17")
+    tmp_product = Product.create(name = "anti-coronovirus", praise = 999999, store = tmp_store)
+    tmp_storage = Storage.create(adress = "fantasy", product = tmp_product)
+    tmp_supply = Supply.create(product = tmp_product, count = 1, date = datetime.datetime(2024, 10, 1, 12, 24))
+    tmp_supply = Supply.create(product = tmp_product, count = 111, date = datetime.datetime(2026, 10, 1, 12, 24))
+    tmp_product = Product.create(name = "mal'chik", praise = 100000*1000000, store = tmp_store)
+    tmp_storage = Storage.create(adress = "u drakoshi", product = tmp_product)
+    tmp_rate = Rate.create(author = "na zabore", stars = 3, product = tmp_product)
+    tmp_supply = Supply.create(product = tmp_product, count = 33, date = datetime.datetime(1999, 10, 1, 12, 24))
+    tmp_cash = Cashier.create(store = tmp_store, name = "Petya", salary = 1)
+
+#Второй магазин
+def initDatabase2():
+    tmp_store = Store.create(name = "RF", adress = "Eurasia")
+    tmp_product = Product.create(store = tmp_store, name = "oil", praise = 1970)
+    tmp_storage = Storage.create(adress = "Ural", product = tmp_product)
+    tmp_rate = Rate.create(author = "Naval'nii", stars = 0, product = tmp_product)
+    tmp_supply = Supply.create(product = tmp_product, count = 10000000000, date = datetime.datetime(2021, 1, 1, 1, 0))
+    tmp_cash = Cashier.create(store = tmp_store, name = "Dima Medvedev", salary = 1000000)
+
+#Заполним всю бд
+def initDatabase():
+    initDatabase1()
+    initDatabase2()
+
+class MyDataBase:
+    def __init__(self, name):
+        #Возьмем бд из файла
+        mainDatabase.init(name)
+        #Названия и так говорящие
+        mainDatabase.connect()
+        mainDatabase.create_tables([Store, Product, Storage, Rate, Supply, Cashier])
+        try:
+            #попробуем заполнить бд если бд уже заполнена то уникальные поля бросят ошибку и мы выйдем
+            initDatabase()
+        except IntegrityError:
+            #Чтоб знать что мы не инициализируем ее. Ну так. Лишним не будет
+            print("database init canceled")
+
+
+    #Имена магазинов хз зачем
+    def stores(self):
+        return [i.name for i in Store.select()]
+
+    #Имена товаров
+    def products(self):
+        return [i.name for i in Product.select()]
+
+    #Имена товаров у которых есть поставки
+    def products_in_supply(self):
+        return {i.product.name : None for i in Supply.select()}.keys()
+
+    #макс зп
+    def max_salary(self):
+        return Cashier.select(fn.MAX(Cashier.salary)).scalar()
+
+    #Добавим че надо
+    def add(self, num, author, prod):
+        finded = Product.get(Product.name == prod)
+        if finded is None:
+            return
+        Rate.create(author = author, stars = num, product = finded)
+
+    #первый запрос
+    def first(self, name):
+        tmp = [(str(i.date), i.count) for i in Supply.select().join(Product).where(Product.name == name)]
+        tmp.append(("all", Supply.select(fn.SUM(Supply.count)).join(Product).where(Product.name == name).scalar()))
+        #Напомню. tmp - Это типа двумерный массив(таблица) ["supplies ...", ...] - Это список имен колонок
+        return MyModel(tmp, ["supplies of '"+name+"'", "count"])
+
+    #Второй запрос
+    def second(self, substr):
+        tmp = [(i.adress, i.product.name, i.product.praise) for i in 
+                Storage.select().join(Product).join(Store).where(Store.name.contains(substr)).order_by(Product.praise)]
+        return MyModel(tmp, ['adress of storages', 'products', 'praise'])
+
+    #Неожиданно но третий запрос
+    def third(self, num, date):
+        tmp = [(i.stars, i.product.name, i.product.store.adress) for i in 
+                Rate.select().join(Product).join(Supply).switch(Product).join(Store).join(Cashier).where((Supply.date > date) & (Cashier.salary < num))]
+        return MyModel(tmp, ['rate', 'product', 'store adress'])
+
+
+
